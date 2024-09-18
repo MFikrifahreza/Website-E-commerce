1d85926c00fdf04f3b3bdcbeb60d2a526efdedec
M.Fizaa <saintsgimang@gmail.com>
2024-09-18 18:14:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,10 +16,11 @@ if (close) {
 
 // Cek status login saat halaman dimuat
 window.onload = function() {
-  const loggedIn = localStorage.getItem("loggedIn");
-  if (!loggedIn && window.location.pathname !== '/login.html') {
-    window.location.href = "login.html"; // Arahkan ke login jika belum login
-  }
+    const loggedIn = localStorage.getItem("loggedIn");
+    console.log("Logged In Status: ", loggedIn); // Debugging
+    if (!loggedIn) {
+        window.location.href = "login.html"; // Arahkan ke login jika belum login
+    }
 };
 
 // Menangani pengiriman form login
